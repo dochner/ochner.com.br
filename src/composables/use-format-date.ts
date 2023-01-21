e816ed9193b2435e397a2a885c73f735b7b4a90d
Douglas Gabriel Ochner <douglas.ochner.ext@spms.min-saude.pt>
2023-01-21 15:11:13 +0000

    feat: clean up repository

--- src/composables/use-format-date.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from 'dayjs'
-
-export function formatDate(d: string | Date) {
-  const date = dayjs(d)
-  if (date.year() === dayjs().year())
-    return date.format('MMM D')
-  return date.format('MMM D, YYYY')
-}
--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const DOMAIN = 'https://ochner.com.br'
