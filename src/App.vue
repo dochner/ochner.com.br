28a71aaee5ee725e28b838f0e1ac05f929f76224
Douglas Ochner <douglas.ochner@gmail.com>
2023-04-22 09:06:41 +0100

    feat: update og tags in website

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@ useHead({
   meta: [
     { property: 'og:title', content: 'Douglas Ochner' },
     { property: 'og:image', content: `${APP_URL}/og-icon.png` },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:description', content: 'I love develop websites and create amazing things' },
     { name: 'description', content: 'Douglas Ochner\'s Portfolio' },
     { name: 'twitter:card', content: 'summary' },
   ],
